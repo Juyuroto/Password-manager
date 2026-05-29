cd8af345fcb4f1f31008410a16b22ef2d1f69517
Alain <alain.corazzini@epitech.eu>
2026-05-29 12:34:21 +0200

    [DELETE] All components is down

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,11 @@
 import "./assets/css/App.css";
 
 // Components
-import Header from "./components/home/header";
-import Main from "./components/home/main";
-import Footer from "./components/home/footer";
-import VaultPage from "./pages/vault";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Main />
-      <Footer />
+
     </div>
   );
 }
--- frontend/src/components/home/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-	return (
-		<footer>
-			<small>© {new Date().getFullYear()} Lockbox</small>
-		</footer>
-	)
-}
-
-export default Footer
